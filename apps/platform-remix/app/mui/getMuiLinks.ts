e22a79b1efebafba97c1a0199186c38c117192f3
Carlos Cruz <me@carcruzcast.com>
2025-02-04 23:43:35 +0000

    feat: add apollo

--- apps/platform-remix/app/mui/getMuiLinks.ts
+++ apps/platform-remix/app/mui/getMuiLinks.ts
@@ -8,8 +8,4 @@ export const getMuiLinks: LinksFunction = () => [
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
-  },
 ];
